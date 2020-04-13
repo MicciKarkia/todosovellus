97d72fe749df37820d458aa40245379ff9658e0b
Michaela Karkia <michaela@karkia.me>
2020-04-13 16:55:24 +0300

    First commit

---
+++ src/main/java/hh/swd20/todosovellus/TodosovellusApplication.java
@@ -0,0 +1,13 @@
+package hh.swd20.todosovellus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosovellusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodosovellusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hh/swd20/todosovellus/TodosovellusApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.swd20.todosovellus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosovellusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
