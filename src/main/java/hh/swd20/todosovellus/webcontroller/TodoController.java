ec766093195858e1aea22f9089b416f79f5d7d26
Michaela Karkia <michaela@karkia.me>
2020-04-23 00:05:43 +0300

    added Controller CORS config

--- src/main/java/hh/swd20/todosovellus/webcontroller/TodoController.java
+++ src/main/java/hh/swd20/todosovellus/webcontroller/TodoController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,6 +47,7 @@ public class TodoController {
 		return "todolist";
 	}
 	
+	@CrossOrigin(origins = "http://localhost:3000")
 	@RequestMapping(value="/todos", method = RequestMethod.GET)
 	public @ResponseBody List<Todo> todoListRest() {
 		return (List<Todo>) todorepository.findAll();
