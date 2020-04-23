5a0daec472bdcd4e8d1ed6524e002bdee1dee4ef
Michaela Karkia <michaela@karkia.me>
2020-04-23 09:16:02 +0300

    CORS config

--- src/main/java/hh/swd20/todosovellus/WebsecurityConfig.java
+++ src/main/java/hh/swd20/todosovellus/WebsecurityConfig.java
@@ -54,8 +54,8 @@ public class WebsecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000/"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST","HEAD"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
